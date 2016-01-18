c093add1898dda38befa91ffe9283a8aeaf53107
Ian London <ianlondondu@gmail.com>
2016-01-18 15:29:05 -0500

    done

--- python/q5_datetime.py
+++ python/q5_datetime.py
@@ -4,10 +4,25 @@
 date_start = '01-02-2013'  
 date_stop = '07-28-2015'   
 
+date_start, date_stop = [dt.datetime.strptime(d, '%m-%d-%Y') for d in (date_start,date_stop)]
+
+day_diff = (date_stop-date_start).days
+print "The difference is {} days".format(day_diff)
+
 ####b)  
 date_start = '12312013'  
 date_stop = '05282015'  
 
+date_start, date_stop = [dt.datetime.strptime(d, '%m%d%Y') for d in (date_start,date_stop)]
+
+day_diff = (date_stop-date_start).days
+print "The difference is {} days".format(day_diff)
+
 ####c)  
 date_start = '15-Jan-1994'  
 date_stop = '14-Jul-2015'  
+
+date_start, date_stop = [dt.datetime.strptime(d, '%d-%b-%Y') for d in (date_start,date_stop)]
+
+day_diff = (date_stop-date_start).days
+print "The difference is {} days".format(day_diff)
