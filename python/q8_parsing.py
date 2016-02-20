107950ead268ba7bcafda1142bd7db54a6c98302
IanLondon <ianlondondu@gmail.com>
2016-02-19 19:34:30 -0500

    q8

--- python/q8_parsing.py
+++ python/q8_parsing.py
@@ -1,19 +1,22 @@
-#The football.csv file contains the results from the English Premier League. 
-# The columns labeled ‘Goals’ and ‘Goals Allowed’ contain the total number of 
-# goals scored for and against each team in that season (so Arsenal scored 79 goals 
-# against opponents, and had 36 goals scored against them). Write a program to read the file, 
-# then print the name of the team with the smallest difference in ‘for’ and ‘against’ goals.
+#The football.csv file contains the results from the English Premier League.
+# The columns labeled Goals and Goals Allowed contain the total number of
+# goals scored for and against each team in that season (so Arsenal scored 79 goals
+# against opponents, and had 36 goals scored against them). Write a program to read the file,
+# then print the name of the team with the smallest difference in for and against goals.
 
 # The below skeleton is optional.  You can use it or you can write the script with an approach of your choice.
 
 
-import csv
+import pandas as pd
+import os
 
-  def read_data(data):
-   # COMPLETE THIS FUNCTION
+data = pd.read_csv(os.path.join(os.path.dirname(__file__), 'football.csv'))
 
-  def get_min_score_difference(self, parsed_data):
-    # COMPLETE THIS FUNCTION
+# add a column of absolute difference btw for and against goals
+data['diff'] = abs(data.Goals - data['Goals Allowed'])
 
-  def get_team(self, index_value, parsed_data):
-    # COMPLETE THIS FUNCTION
+# get the row with the lowest difference as a Series
+min_diff_team = data.iloc[data['diff'].idxmin(axis=0)]
+
+print "Team with the smallest difference in 'for' and 'against' goals:"
+print min_diff_team.Team
